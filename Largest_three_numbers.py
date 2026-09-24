b057e7ee60849028046043ffa4610db9cb6e0203
canzonadzka <canzonadzka@gmail.com>
2026-09-24 10:50:47 +0700

    mencari angka terbesar

--- Largest_three_numbers.py
+++ Largest_three_numbers.py
@@ -2,6 +2,7 @@ angka1 = float(input("Masukkan angka pertama: "))
 angka2 = float(input("Masukkan angka kedua: "))
 angka3 = float(input("Masukkan angka ketiga: "))
 
+
 if angka1 > angka2 and angka1 > angka3:
     print("Angka terbesar adalah:", angka1)
 elif angka2 > angka1 and angka2 > angka3:
